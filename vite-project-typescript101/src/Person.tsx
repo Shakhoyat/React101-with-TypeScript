2b28611d0292f06440e621c63314c6c27fc9c342
Shakhoyat <shakoyatsujon@gmail.com>
2025-06-04 10:11:46 +0600

    Add Person component and refactor App component

---
+++ vite-project-typescript101/src/Person.tsx
@@ -0,0 +1,17 @@
+interface PersonProps = {
+    name: string;
+    age: number;
+    isStudent: boolean;
+}
+
+const Person = (props: PersonProps) => {
+    return (
+        <div>
+            <p>
+                Name: {props.name}, Age: {props.age}, Is Student: {props.isStudent ? "Yes" : "No"}
+            </p>
+        </div>
+    );
+};
+
+export default Person;
